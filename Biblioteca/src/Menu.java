0ba53e9b4806adb0aa1750a52f403c927d1cafa7
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-26 13:07:41 +0100

    menu mejor

--- Biblioteca/src/Menu.java
+++ Biblioteca/src/Menu.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Menu {
 
@@ -9,37 +8,14 @@ public class Menu {
 		final int OPCION_TRES = 3;
 		final int SALIR = 0;
 
-		Scanner scan = new Scanner(System.in);
-		int opcion_menu;
-
-		do {
+		
+	
 			System.out.println("------MENU-------");
 			System.out.println(OPCION_UNO + ". Gestionar Libros");
 			System.out.println(OPCION_DOS + ". Gestionar Socios");
 			System.out.println(OPCION_TRES + ". Gestionar Prestamos");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
-			opcion_menu = Integer.parseInt(scan.nextLine());
-
-			switch (opcion_menu) {
-			case OPCION_UNO:
-				
-				break;
-			case OPCION_DOS:
-				
-				break;
-			case OPCION_TRES:
-				
-				break;
-			case SALIR:
-				System.out.println("ADIOS");
-				break;
-			default:
-				System.out.println("Opcion incorrecta!");
-			}
-
-		} while (opcion_menu != SALIR);
-		scan.close();
 	}
 
 	static public void mostrarMenuLibros() {
@@ -49,37 +25,12 @@ public class Menu {
 		final int OPCION_TRES = 3;
 		final int SALIR = 0;
 
-		Scanner scan = new Scanner(System.in);
-		int opcion_menu;
-
-		do {
 			System.out.println("------MENU-------");
 			System.out.println(OPCION_UNO + ". Insertar Libro");
 			System.out.println(OPCION_DOS + ". Eliminar Libro");
 			System.out.println(OPCION_TRES + ". Ver Libro");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
-			opcion_menu = Integer.parseInt(scan.nextLine());
-
-			switch (opcion_menu) {
-			case OPCION_UNO:
-				
-				break;
-			case OPCION_DOS:
-				
-				break;
-			case OPCION_TRES:
-				
-				break;
-			case SALIR:
-				System.out.println("ADIOS");
-				break;
-			default:
-				System.out.println("Opcion incorrecta!");
-			}
-
-		} while (opcion_menu != SALIR);
-		scan.close();
 	}
 
 	static public void mostrarMenuSocios() {
@@ -87,29 +38,10 @@ public class Menu {
 		final int OPCION_UNO = 1;
 		final int SALIR = 0;
 
-		Scanner scan = new Scanner(System.in);
-		int opcion_menu;
-
-		do {
 			System.out.println("------MENU-------");
 			System.out.println(OPCION_UNO + ". Ver Socios");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
-			opcion_menu = Integer.parseInt(scan.nextLine());
-
-			switch (opcion_menu) {
-			case OPCION_UNO:
-				
-				break;
-			case SALIR:
-				System.out.println("ADIOS");
-				break;
-			default:
-				System.out.println("Opcion incorrecta!");
-			}
-
-		} while (opcion_menu != SALIR);
-		scan.close();
 	}
 
 	static public void mostrarMenuPrestamos() {
@@ -117,28 +49,9 @@ public class Menu {
 		final int OPCION_UNO = 1;
 		final int SALIR = 0;
 
-		Scanner scan = new Scanner(System.in);
-		int opcion_menu;
-
-		do {
 			System.out.println("------MENU-------");
 			System.out.println(OPCION_UNO + ". Realizar Prestamo");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
-			opcion_menu = Integer.parseInt(scan.nextLine());
-
-			switch (opcion_menu) {
-			case OPCION_UNO:
-				
-				break;
-			case SALIR:
-				System.out.println("ADIOS");
-				break;
-			default:
-				System.out.println("Opcion incorrecta!");
-			}
-
-		} while (opcion_menu != SALIR);
-		scan.close();
 	}
 }
