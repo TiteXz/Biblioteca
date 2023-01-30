ab4fe6b61910dc30c4ddb5238d076d39606ae780
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-30 14:09:50 +0100

    conectar y cerrar

--- Biblioteca/src/GestorBBDD.java
+++ Biblioteca/src/GestorBBDD.java
@@ -7,20 +7,24 @@ import java.util.Scanner;
 public class GestorBBDD extends Conector{
 	
 	
-	public void insertarLibros(Libro libro) throws SQLException {
+	public void insertarLibros(Libro libro) throws SQLException, ClassNotFoundException {
+		super.conectar();
 			PreparedStatement pst = con.prepareStatement("INSERT INTO libros VALUES (null,?,?,?)");
 			pst.setString(1,libro.getTitulo());
 			pst.setString(2,libro.getAutor());
 			pst.setInt(1,libro.getNum_pag());
 			
 			pst.execute();
+		super.cerrar();	
 	}
 	
-	public void eliminarLibro(int id) throws SQLException {
-		PreparedStatement pst = con.prepareStatement("DELETE FROM libros WHERE id = ?");
-		pst.setInt(1, id);
+	public void eliminarLibro(int id) throws SQLException, ClassNotFoundException {
+		super.conectar();
+			PreparedStatement pst = con.prepareStatement("DELETE FROM libros WHERE id = ?");
+			pst.setInt(1, id);
 		
-		pst.execute();
+			pst.execute();
+		super.cerrar();		
 	}
 
 	public Libro getLibro(int id) {
