e4a73daf9f53d252f51abb1ff0ad4ffdf64f9f86
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-30 12:58:17 +0100

    menu de menus

--- Biblioteca/src/Menu.java
+++ Biblioteca/src/Menu.java
@@ -1,13 +1,52 @@
 import java.text.ParseException;
+import java.util.Scanner;
 
 public class Menu {
 
 	public void run() throws ParseException {
 		
-		mostrarMenuPrincipal();
-		mostrarMenuLibros();
-		mostrarMenuSocios();
-		mostrarMenuPrestamos();
+		//declaracion de constantes (final) para las opciones
+		final int OPCION_UNO = 1;
+		final int OPCION_DOS = 2;
+		final int OPCION_TRES = 3;
+		final int OPCION_CUATRO = 4;
+		final int SALIR = 0;
+
+		Scanner scan = new Scanner(System.in);
+		int opcion_menu;
+
+		do {
+			System.out.println("------MENU-------");
+			System.out.println(OPCION_UNO + ". Mostrar el menu principal ");
+			System.out.println(OPCION_DOS + ". Mostrar el menu de los libros");
+			System.out.println(OPCION_TRES + ". Mostrar el menu de los socios");
+			System.out.println(OPCION_CUATRO + ". Mostrar el menu de los prestamos");
+			System.out.println(SALIR + ". Salir");
+			System.out.println("Elije una de las opciones");
+			opcion_menu = Integer.parseInt(scan.nextLine());
+
+			switch (opcion_menu) {
+			case OPCION_UNO:
+				mostrarMenuPrincipal();
+				break;
+			case OPCION_DOS:
+				mostrarMenuLibros();
+				break;
+			case OPCION_TRES:
+				mostrarMenuSocios();
+				break;
+			case OPCION_CUATRO:
+				mostrarMenuPrestamos();
+				break;
+			case SALIR:
+				System.out.println("ADIOS");
+				break;
+			default:
+				System.out.println("Opcion incorrecta!");
+			}
+
+		} while (opcion_menu != SALIR);
+		scan.close();
 	}
 	
 	static public void mostrarMenuPrincipal() {
