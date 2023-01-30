2e07c4ba35b1e7f8e1fd17ac689e18f8219288f4
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-30 14:03:57 +0100

    insertar

--- Biblioteca/src/GestorBBDD.java
+++ Biblioteca/src/GestorBBDD.java
@@ -7,34 +7,26 @@ import java.util.Scanner;
 public class GestorBBDD extends Conector{
 	
 	
-	public void insertarLibros(int id) throws SQLException {
-			PreparedStatement Pst = con.prepareStatement("INSERT INTO libros VALUES (null,?,?,?)");
-			Libro libro = new Libro();
-			Scanner sc = new Scanner(System.in);
+	public void insertarLibros(Libro libro) throws SQLException {
+			PreparedStatement pst = con.prepareStatement("INSERT INTO libros VALUES (null,?,?,?)");
+			pst.setString(1,libro.getTitulo());
+			libro.setAutor(null);
+			libro.setNum_pag(null);
 			
-			
-			System.out.println("introduce el titulo del libro");
-			libro.setTitulo( sc.nextLine());
-			Pst.setString(1,libro.getTitulo());
-			System.out.println("introduce el nombre del autor del libro");
-			libro.setAutor( sc.nextLine());
-			Pst.setString(2, libro.getAutor());
-			System.out.println("introduce el número de paginas del libro");
-			libro.setNum_pag( sc.nextLine());
-			Pst.setString(3, libro.getNum_pag());
-
-			Pst.execute();
-			sc.close();
+			pst.execute();
 	}
 	
-public void eliminarLibro(int id) {
+	public void eliminarLibro(int id) throws SQLException {
+		PreparedStatement Pst = con.prepareStatement("DELETE FROM libros WHERE id = ?");
 		
+		Pst.execute();
 	}
 
-public Libro getLibro(int id) {
+	public Libro getLibro(int id) {
+		
 	return null;
 	
-}
+	}
 	
 }
 
