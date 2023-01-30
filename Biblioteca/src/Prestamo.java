35219047577d6f3853562288b06249c5b14e12b5
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-30 13:15:55 +0100

    Creado, constructor, getters y setters

---
+++ Biblioteca/src/Prestamo.java
@@ -0,0 +1,51 @@
+
+public class Prestamo {
+	
+	private int id;
+	private String id_socio;
+	private String fecha;
+	private String devuelto;
+	
+	public Prestamo() {}
+	
+	public Prestamo(int id, String id_socio, String fecha, String devuelto) {
+		this.id = id;
+		this.id_socio = id_socio;
+		this.fecha = fecha;
+		this.devuelto = devuelto;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getId_socio() {
+		return id_socio;
+	}
+
+	public void setId_socio(String id_socio) {
+		this.id_socio = id_socio;
+	}
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getDevuelto() {
+		return devuelto;
+	}
+
+	public void setDevuelto(String devuelto) {
+		this.devuelto = devuelto;
+	}
+	
+
+}
