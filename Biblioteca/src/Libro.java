9a3afafb325e66238e0538659e96aa489da88c66
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-30 14:04:17 +0100

    int num_paginas corregido

--- Biblioteca/src/Libro.java
+++ Biblioteca/src/Libro.java
@@ -4,11 +4,11 @@ public class Libro {
 	private int id;
 	private String titulo;
 	private String autor;
-	private String num_pag;
+	private int num_pag;
 	
 	public Libro() {}
 	
-	public Libro(int id, String titulo, String autor, String num_pag) {
+	public Libro(int id, String titulo, String autor, int num_pag) {
 		this.id = id;
 		this.titulo = titulo;
 		this.autor = autor;
@@ -39,11 +39,11 @@ public class Libro {
 		this.autor = autor;
 	}
 
-	public String getNum_pag() {
+	public int getNum_pag() {
 		return num_pag;
 	}
 
-	public void setNum_pag(String num_pag) {
+	public void setNum_pag(int num_pag) {
 		this.num_pag = num_pag;
 	}
 	
