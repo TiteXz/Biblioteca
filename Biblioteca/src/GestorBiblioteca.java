d31960729cf41a47529ab09308afb4c71f3a5801
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-30 12:51:31 +0100

    run

--- Biblioteca/src/GestorBiblioteca.java
+++ Biblioteca/src/GestorBiblioteca.java
@@ -1,6 +1,11 @@
+import java.text.ParseException;
 
 public class GestorBiblioteca {
 
-	Run run = new Run();
-	
+	public static void main(String[] args) throws ParseException {
+		Menu gB = new Menu();
+		gB.run();
+		//gb viene de GestorBliblioteca
+	}
+
 }
