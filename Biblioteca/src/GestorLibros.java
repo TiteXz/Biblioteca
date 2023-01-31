4eb994520a2139e5ae97f2f4241ec19c8b78c9e9
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-31 10:03:28 +0100

    terminado

--- Biblioteca/src/GestorLibros.java
+++ Biblioteca/src/GestorLibros.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
 
 public class GestorLibros {
 
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		int opcion_menu;
+		
+		Menu.mostrarMenuLibros();
+		opcion_menu = Integer.parseInt(sc.nextLine());
+		
+		switch (opcion_menu) {
+		case Menu.INSERTAR_LIBRO:
+			Menu.mostrarMenuPrincipal();
+			break;
+		case Menu.ELIMINAR_LIBRO:
+			Menu.mostrarMenuLibros();
+			break;
+		case Menu.MOSTRAR_LIBRO:
+			Menu.mostrarMenuSocios();
+			break;
+		default:
+			System.out.println("Opcion incorrecta!");
+		}
+	
+	sc.close();
+	}
 }
