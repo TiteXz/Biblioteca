26d8a06b0734a63b6274ea218f34969669b5b444
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-31 09:00:02 +0100

    Menus mejorados

--- Biblioteca/src/Menu.java
+++ Biblioteca/src/Menu.java
@@ -6,10 +6,10 @@ public class Menu {
 	public void run() throws ParseException {
 		
 		//declaracion de constantes (final) para las opciones
-		final int OPCION_UNO = 1;
-		final int OPCION_DOS = 2;
-		final int OPCION_TRES = 3;
-		final int OPCION_CUATRO = 4;
+		final int MENU_PRINCIPAL = 1;
+		final int MENU_LIBROS = 2;
+		final int MENU_SOCIOS = 3;
+		final int MENU_PRESTAMOS = 4;
 		final int SALIR = 0;
 
 		Scanner scan = new Scanner(System.in);
@@ -17,25 +17,25 @@ public class Menu {
 
 		do {
 			System.out.println("------MENU-------");
-			System.out.println(OPCION_UNO + ". Mostrar el menu principal ");
-			System.out.println(OPCION_DOS + ". Mostrar el menu de los libros");
-			System.out.println(OPCION_TRES + ". Mostrar el menu de los socios");
-			System.out.println(OPCION_CUATRO + ". Mostrar el menu de los prestamos");
+			System.out.println(MENU_PRINCIPAL + ". Mostrar el menu principal ");
+			System.out.println(MENU_LIBROS + ". Mostrar el menu de los libros");
+			System.out.println(MENU_SOCIOS + ". Mostrar el menu de los socios");
+			System.out.println(MENU_PRESTAMOS + ". Mostrar el menu de los prestamos");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
 			opcion_menu = Integer.parseInt(scan.nextLine());
 
 			switch (opcion_menu) {
-			case OPCION_UNO:
+			case MENU_PRINCIPAL:
 				mostrarMenuPrincipal();
 				break;
-			case OPCION_DOS:
+			case MENU_LIBROS:
 				mostrarMenuLibros();
 				break;
-			case OPCION_TRES:
+			case MENU_SOCIOS:
 				mostrarMenuSocios();
 				break;
-			case OPCION_CUATRO:
+			case MENU_PRESTAMOS:
 				mostrarMenuPrestamos();
 				break;
 			case SALIR:
@@ -51,54 +51,54 @@ public class Menu {
 	
 	static public void mostrarMenuPrincipal() {
 		//declaracion de constantes (final) para las opciones
-		final int OPCION_UNO = 1;
-		final int OPCION_DOS = 2;
-		final int OPCION_TRES = 3;
+		final int GESTIONAR_LIBROS = 1;
+		final int GESTIONAR_SOCIOS = 2;
+		final int GESTIONAR_PRESTAMOS = 3;
 		final int SALIR = 0;
 
 		
 	
 			System.out.println("------MENU-------");
-			System.out.println(OPCION_UNO + ". Gestionar Libros");
-			System.out.println(OPCION_DOS + ". Gestionar Socios");
-			System.out.println(OPCION_TRES + ". Gestionar Prestamos");
+			System.out.println(GESTIONAR_LIBROS + ". Gestionar Libros");
+			System.out.println(GESTIONAR_SOCIOS + ". Gestionar Socios");
+			System.out.println(GESTIONAR_PRESTAMOS + ". Gestionar Prestamos");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
 	}
 
 	static public void mostrarMenuLibros() {
 		//declaracion de constantes (final) para las opciones
-		final int OPCION_UNO = 1;
-		final int OPCION_DOS = 2;
-		final int OPCION_TRES = 3;
+		final int INSERTAR_LIBRO = 1;
+		final int ELIMINAR_LIBRO = 2;
+		final int MOSTRAR_LIBRO = 3;
 		final int SALIR = 0;
 
 			System.out.println("------MENU-------");
-			System.out.println(OPCION_UNO + ". Insertar Libro");
-			System.out.println(OPCION_DOS + ". Eliminar Libro");
-			System.out.println(OPCION_TRES + ". Ver Libro");
+			System.out.println(INSERTAR_LIBRO + ". Insertar Libro");
+			System.out.println(ELIMINAR_LIBRO + ". Eliminar Libro");
+			System.out.println(MOSTRAR_LIBRO + ". Ver Libro");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
 	}
 
 	static public void mostrarMenuSocios() {
 		//declaracion de constantes (final) para las opciones
-		final int OPCION_UNO = 1;
+		final int VER_SOCIOS = 1;
 		final int SALIR = 0;
 
 			System.out.println("------MENU-------");
-			System.out.println(OPCION_UNO + ". Ver Socios");
+			System.out.println(VER_SOCIOS + ". Ver Socios");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
 	}
 
 	static public void mostrarMenuPrestamos() {
 		//declaracion de constantes (final) para las opciones
-		final int OPCION_UNO = 1;
+		final int REALIZAR_PRESTAMO = 1;
 		final int SALIR = 0;
 
 			System.out.println("------MENU-------");
-			System.out.println(OPCION_UNO + ". Realizar Prestamo");
+			System.out.println(REALIZAR_PRESTAMO + ". Realizar Prestamo");
 			System.out.println(SALIR + ". Salir");
 			System.out.println("Elije una de las opciones");
 	}
