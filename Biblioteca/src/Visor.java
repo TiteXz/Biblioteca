75cb28e04f7def1fc23a2480216028bb57f41747
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-31 08:52:52 +0100

    mejorado y mostrarMensaje

--- Biblioteca/src/Visor.java
+++ Biblioteca/src/Visor.java
@@ -1,28 +1,21 @@
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Visor extends Conector{
 	
 	ArrayList<Libro> libros = new ArrayList<Libro>();
 
-	public static void mostrarLibros(ArrayList<Libro>libros) throws SQLException {
-		PreparedStatement Pst = con.prepareStatement("SELECT * FROM libros ");
-
-		 ResultSet resultado = Pst.executeQuery();
-			 while(resultado.next()) {
-			 System.out.println(resultado.getInt(1)+ " - " +resultado.getString(2)+ " - " +resultado.getString(3)+ " - " +resultado.getInt(4));
-	 
-			 }	
+	public static void mostrarLibros(ArrayList<Libro>libros) {
+		for (Libro libro : libros) {
+			System.out.println(libro);
+		}
+		
 	}
-	
 
 	public static void mostrarLibro(Libro libro) {
-		System.out.println(libro.getId()+"--"+libro.getTitulo()+"--"+libro.getAutor()+"--"+libro.getNum_pag());
+		System.out.println(libro);
 	}
 	
 	public static void mostrarMensaje(String mensaje) {
-		
+		System.out.println(mensaje);
 	}
 }
