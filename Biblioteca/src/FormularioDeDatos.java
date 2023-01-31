cb2ddd609409d50aa3847209d118b188677a0f4e
Iker Zamorano <Iker Zamorano@HZ321161.H012108.NET>
2023-01-31 09:16:06 +0100

    modificarLibro

--- Biblioteca/src/FormularioDeDatos.java
+++ Biblioteca/src/FormularioDeDatos.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 
 public class FormularioDeDatos {
-
-	Scanner sc = new Scanner(System.in);
 	
 	public static Libro pedirDatosLibro(Scanner sc) {
 		Libro libro = new Libro();
@@ -14,6 +12,20 @@ public class FormularioDeDatos {
 		System.out.println("¿Cuantas paginas tiene el libro?");
 		libro.setNum_pag(Integer.parseInt(sc.nextLine()));
 		
+		return libro;
+		
+	}
+	
+	public static Libro modificarDatosLibro(Libro libro, Scanner sc) {
+		
+		System.out.println("Introduce el id del libro que quieras modificar");
+		libro.setId(Integer.parseInt(sc.nextLine()));
+		System.out.println("Introduce el nuevo titulo para el libro");
+		libro.setTitulo(sc.nextLine());
+		System.out.println("Introduce el nuevo autor del libro");
+		libro.setAutor(sc.nextLine());
+		System.out.println("Introduce el numero de paginas del nuevo libro");
+		libro.setNum_pag(Integer.parseInt(sc.nextLine()));
 		return libro;
 		
 	}
