d232f81dbed089653a247635c39ae8800a13c357
Iker <ikcfi@plaiaundi.net>
2023-02-01 14:26:47 +0100

    soucionado

--- Biblioteca/src/Main.java
+++ Biblioteca/src/Main.java
@@ -1,8 +1,9 @@
+import java.sql.SQLException;
 import java.text.ParseException;
 
 public class Main {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, ClassNotFoundException, SQLException {
 		GestorBiblioteca gB = new GestorBiblioteca();
 		gB.run();
 		//gb viene de GestorBliblioteca
