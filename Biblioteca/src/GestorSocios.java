07d26f855c52a7d1682cc1c8328219cd98b44bcb
Iker <ikcfi@plaiaundi.net>
2023-02-01 13:44:58 +0100

    terminado

--- Biblioteca/src/GestorSocios.java
+++ Biblioteca/src/GestorSocios.java
@@ -1,17 +1,29 @@
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class GestorSocios {
-	public void run() {
+	public void run() throws ClassNotFoundException, SQLException {
 		Scanner sc = new Scanner(System.in);
 		int opcion_menu;
+		GestorBBDD gbd = new GestorBBDD();
+		Socio socio = new Socio();
 		
 		Menu.mostrarMenuLibros();
 		opcion_menu = Integer.parseInt(sc.nextLine());
 		
 		do {
 		switch (opcion_menu) {
+		case Menu.INSERTAR_SOCIO:
+			gbd.insertarSocio(FormularioDeDatos.pedirDatosSocio(sc));;
+			break;
+		case Menu.ELIMINAR_SOCIO:
+			gbd.eliminarSocio(FormularioDeDatos.pedirIdSocio(sc));;
+			break;
+		case Menu.MODIFICAR_SOCIO:
+			FormularioDeDatos.modificarDatosSocio(socio, sc);
+			break;
 		case Menu.VER_SOCIOS:
-			Menu.mostrarMenuPrincipal();
+			gbd.getSocio(FormularioDeDatos.pedirIdSocio(sc));
 			break;
 		default:
 			System.out.println("Opcion incorrecta!");
