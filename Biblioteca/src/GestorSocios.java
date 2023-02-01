f72c915e0cb568de0bb945c7d1a5d62ae2600026
Iker <ikcfi@plaiaundi.net>
2023-02-01 12:47:02 +0100

    terminada la base

--- Biblioteca/src/GestorSocios.java
+++ Biblioteca/src/GestorSocios.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
 
 public class GestorSocios {
-
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		int opcion_menu;
+		
+		Menu.mostrarMenuLibros();
+		opcion_menu = Integer.parseInt(sc.nextLine());
+		
+		switch (opcion_menu) {
+		case Menu.GESTIONAR_LIBROS:
+			Menu.mostrarMenuPrincipal();
+			break;
+		case Menu.GESTIONAR_SOCIOS:
+			Menu.mostrarMenuLibros();
+			break;
+		case Menu.GESTIONAR_PRESTAMOS:
+			Menu.mostrarMenuSocios();
+			break;
+		default:
+			System.out.println("Opcion incorrecta!");
+		}
+	
+	sc.close();
+	}
 }
