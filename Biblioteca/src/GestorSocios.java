e26b5044a4363ebff7c7ee9c1eb8c93aa87a86be
Iker <ikcfi@plaiaundi.net>
2023-02-06 13:11:02 +0100

    perfecto

--- Biblioteca/src/GestorSocios.java
+++ Biblioteca/src/GestorSocios.java
@@ -9,29 +9,37 @@ public class GestorSocios {
 		
 	
 		
-	
-			Menu.mostrarMenuLibros();
+	do {
+			Menu.mostrarMenuSocios();
 			opcion_menu = Integer.parseInt(sc.nextLine());
 			
 		switch (opcion_menu) {
 		case Menu.INSERTAR_SOCIO:
+			gbd.conectar();
 			gbd.insertarSocio(FormularioDeDatos.pedirDatosSocio(sc));;
+			gbd.cerrar();
 			break;
 		case Menu.ELIMINAR_SOCIO:
+			gbd.conectar();
 			gbd.eliminarSocio(FormularioDeDatos.pedirIdSocio(sc));;
+			gbd.cerrar();
 			break;
 		case Menu.MODIFICAR_SOCIO:
+			gbd.conectar();
 			FormularioDeDatos.modificarDatosSocio(socio, sc);
+			gbd.cerrar();
 			break;
 		case Menu.VER_SOCIOS:
+			gbd.conectar();
 			gbd.getSocio(FormularioDeDatos.pedirIdSocio(sc));
+			gbd.cerrar();
 			break;
 		default:
 			System.out.println("Opcion incorrecta!");
 		}
 		
 			
-	
+	}while(opcion_menu !=0);
 	sc.close();
 	}
 }
